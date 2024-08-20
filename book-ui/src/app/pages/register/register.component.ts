5f1dfa474d54b797ca4447d4365645a8529b59c6
ajay <ajaysinghrana8894@gmail.com>
2024-08-20 09:08:11 +0530

    working register component

--- book-ui/src/app/pages/register/register.component.ts
+++ book-ui/src/app/pages/register/register.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import {RegistrationRequest} from "../../services/models/registration-request";
-import { ReactiveFormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatButton} from "@angular/material/button";
 import {NgForOf, NgIf} from "@angular/common";
-import {RouterLink} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {AuthenticationService} from "../../services/services/authentication.service";
+import {TokenService} from "../../services/models/token/token.service";
 
 @Component({
   selector: 'app-register',
@@ -24,13 +26,39 @@ import {RouterLink} from "@angular/router";
     MatLabel,
     NgForOf,
     NgIf,
-    RouterLink
+    RouterLink,
+    FormsModule
   ],
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
 registerRequest: RegistrationRequest={email:'',firstname:'',lastname:'',password:''};
 errorMsg:Array<string>=[];
+
+constructor(private router:Router,private authService:AuthenticationService,private tokenService: TokenService) {
+  }
+
+
+register(){
+  this.errorMsg=[];
+  this.authService.register({
+    body:this.registerRequest
+  }).subscribe({
+    next:():void=>{
+      this.router.navigate(['activate-account']);
+    },
+    error:(err):void =>{
+      console.log(err);
+      if (err.error.validationErrors){
+        this.errorMsg=err.error.validationErrors;
+      }
+      else{
+        this.errorMsg.push(err.error.errorCode);
+      }
+    }
+  })
+}
+
 
 
 
